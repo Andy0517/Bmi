1d216b820f5aaec21b7c67ea3545798408180e8a
Andy0517 <ad870517@gmail.com>
2017-10-31 12:08:24 +0800

    Update MainActivity.java
    alert

--- app/src/main/java/com/lin/bmi/MainActivity.java
+++ app/src/main/java/com/lin/bmi/MainActivity.java
@@ -13,13 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void bmi(View view){
+     public void bmi(View view){
         Log.d("MainActivit","testing bmi method");
         EditText edWeight = (EditText) findViewById(R.id.ed_weight);
         EditText edHeight = (EditText) findViewById(R.id.ed_height);
         float weight = Float.parseFloat(edWeight.getText().toString());
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight/(height*height);
+        new AlertDialog.Builder(this)
+                .setTitle("mytitle")
+                .setMessage("Your bmi:"+bmi)
+                .setPositiveButton("OK",null)
+                .setNegativeButton("CANCEL",null)
+               // .setNeutralButton("hi",null)
+                .show();
         Log.d("MainActivity","bmi:"+bmi);
+        Toast.makeText(this,"Your bmi:"+bmi,Toast.LENGTH_LONG).show();
     }
 }
