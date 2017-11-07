1a7dd31276a58a8ba78d08f4fc1b65d07ec2dd20
Andy0517 <ad870517@gmail.com>
2017-11-07 10:04:49 +0800

    resource string

--- app/src/main/java/com/lin/bmi/MainActivity.java
+++ app/src/main/java/com/lin/bmi/MainActivity.java
@@ -1,10 +1,12 @@
 package com.lin.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,22 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // getResources().getString(R.string.app_name);
     }
      public void bmi(View view){
-        Log.d("MainActivit","testing bmi method");
+        //Log.d("MainActivit","testing bmi method");
         EditText edWeight = (EditText) findViewById(R.id.ed_weight);
         EditText edHeight = (EditText) findViewById(R.id.ed_height);
         float weight = Float.parseFloat(edWeight.getText().toString());
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight/(height*height);
         new AlertDialog.Builder(this)
-                .setTitle("mytitle")
-                .setMessage("Your bmi:"+bmi)
-                .setPositiveButton("OK",null)
-                .setNegativeButton("CANCEL",null)
+                .setTitle(R.string.bmi_titile)
+                .setMessage(getString(R.string.your_bmi)+bmi)
+                .setPositiveButton(R.string.ok,null)
+                .setNegativeButton(R.string.cancel,null)
                // .setNeutralButton("hi",null)
                 .show();
-        Log.d("MainActivity","bmi:"+bmi);
-        Toast.makeText(this,"Your bmi:"+bmi,Toast.LENGTH_LONG).show();
+       /* Log.d("MainActivity","bmi:"+bmi);*/
+        Toast.makeText(this,getString(R.string.your_bmi)+bmi+":"+bmi, Toast.LENGTH_LONG).show();
     }
 }
